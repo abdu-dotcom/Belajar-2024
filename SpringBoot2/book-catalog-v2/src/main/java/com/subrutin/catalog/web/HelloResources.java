c361e6ac9b87eda4d2676edb3caa279b081e9bc8
abduh <muhammadabdu089@gmail.com>
2024-09-09 14:40:09 +0700

    Menerapkan properties spring

--- SpringBoot2/book-catalog-v2/src/main/java/com/subrutin/catalog/web/HelloResources.java
+++ SpringBoot2/book-catalog-v2/src/main/java/com/subrutin/catalog/web/HelloResources.java
@@ -1,8 +1,14 @@
 package com.subrutin.catalog.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloResources {
 
-    public String helloword() {
-        return "Hellow Word!";
+    @GetMapping("/hello")
+    public String helloword(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return String.format("Hellow %s!",name);
     }
 }
