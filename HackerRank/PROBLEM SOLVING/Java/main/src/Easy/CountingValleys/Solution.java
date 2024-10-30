c76ed0ab2e1afc515a33e23a3ffb66f46b4130b9
abduh <muhammadabdu089@gmail.com>
2024-10-30 13:35:34 +0700

    fix source code folder

---
+++ HackerRank/PROBLEM SOLVING/Java/main/src/Easy/CountingValleys/Solution.java
@@ -0,0 +1,73 @@
+package CountingValleys;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+        // Write your code here
+        char[] charPathArray = path.toCharArray();
+
+        int counthStep = 0;
+        int countValleyPass = 0;
+
+        for (int i = 0; i < charPathArray.length; i++) {
+            char step = charPathArray[i];
+
+            Boolean positiveStep = false;
+
+            if (counthStep > 0) positiveStep = true;
+            
+            if (step == 'U') counthStep = counthStep + 1;
+            if (step == 'D')  counthStep = counthStep - 1;
+
+            if (positiveStep == false){
+                if (counthStep == 0) {
+                    countValleyPass = countValleyPass + 1;
+                }
+            }
+
+        }
+
+        return countValleyPass;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
