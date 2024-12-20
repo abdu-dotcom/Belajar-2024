b72f97604530024d00dda312a091bbd169097a8a
abduh <muhammadabdu089@gmail.com>
2024-12-20 14:59:06 +0700

    Using Java Record as DTO in Banking App

--- Spring Boot/Udemy/[NEW] Building Microservices with Spring Boot & Spring Cloud/Project/banking-app/src/main/java/net/javaguides/banking/dto/AccountDto.java
+++ Spring Boot/Udemy/[NEW] Building Microservices with Spring Boot & Spring Cloud/Project/banking-app/src/main/java/net/javaguides/banking/dto/AccountDto.java
@@ -1,13 +1,3 @@
 package net.javaguides.banking.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@Data
-public class AccountDto {
-    private Long id;
-    private String accountHolderName;
-    private double balance;
-}
+public record AccountDto(Long id, String accountHolderName, double balance) {}
--- Spring Boot/Udemy/[NEW] Building Microservices with Spring Boot & Spring Cloud/Project/banking-app/src/main/java/net/javaguides/banking/mapper/AccountMapper.java
+++ Spring Boot/Udemy/[NEW] Building Microservices with Spring Boot & Spring Cloud/Project/banking-app/src/main/java/net/javaguides/banking/mapper/AccountMapper.java
@@ -7,7 +7,7 @@ public class AccountMapper {
 
   public static Account mapToAccount(AccountDto accountDto) {
     return new Account(
-        accountDto.getId(), accountDto.getAccountHolderName(), accountDto.getBalance());
+        accountDto.id(), accountDto.accountHolderName(), accountDto.balance());
   }
 
   public static AccountDto mapToAccountDto(Account account) {
