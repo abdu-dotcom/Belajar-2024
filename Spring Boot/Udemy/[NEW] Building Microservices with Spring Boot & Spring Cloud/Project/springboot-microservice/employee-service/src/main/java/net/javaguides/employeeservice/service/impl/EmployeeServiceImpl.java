3e07e42f05044e3e20d12e590e7072f9aa343875
abduh <muhammadabdu089@gmail.com>
2025-01-06 14:54:26 +0700

    108. Microservices Communication using RestTemplate

--- Spring Boot/Udemy/[NEW] Building Microservices with Spring Boot & Spring Cloud/Project/springboot-microservice/employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
+++ Spring Boot/Udemy/[NEW] Building Microservices with Spring Boot & Spring Cloud/Project/springboot-microservice/employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,19 +1,25 @@
 package net.javaguides.employeeservice.service.impl;
 
 import lombok.AllArgsConstructor;
+import net.javaguides.employeeservice.dto.APIResponseDto;
+import net.javaguides.employeeservice.dto.DepartmentDto;
 import net.javaguides.employeeservice.dto.EmployeeDto;
 import net.javaguides.employeeservice.entity.Employee;
 import net.javaguides.employeeservice.exception.ResourceNotFoundException;
 import net.javaguides.employeeservice.mapper.EmployeeMapper;
 import net.javaguides.employeeservice.repository.EmployeeRepository;
 import net.javaguides.employeeservice.service.EmployeeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
+
+    private RestTemplate restTemplate;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -25,12 +31,25 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
-    public EmployeeDto getEmployeeById(Long employeeId) {
+    public APIResponseDto getEmployeeById(Long employeeId) {
+
         Employee employee =
             employeeRepository
                 .findById(employeeId)
                 .orElseThrow(
                     () -> new ResourceNotFoundException("Department", "departmentCode", employeeId));
-        return EmployeeMapper.INSTANCE.employeeToEmployeeDto(employee);
+
+        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/department/" + employee.getDepartmentCode(),
+                DepartmentDto.class);
+
+        EmployeeDto employeeDto = EmployeeMapper.INSTANCE.employeeToEmployeeDto(employee);
+
+        DepartmentDto departmentDto = responseEntity.getBody();
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDepartment(departmentDto);
+
+        return apiResponseDto;
       }
 }
